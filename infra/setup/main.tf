572c506e6ffc6df4664a1052ec2aac0ba904c327
Kaustav Chanda <kstvkmrchanda2@gmail.com>
2025-09-23 19:40:53 +0530

    create terraform project

---
+++ infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "kc-recipe-api-tf-state"
+    key            = "tf-state-setup"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "kc-recipe-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}

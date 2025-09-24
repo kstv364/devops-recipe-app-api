db9bf0570643eab2fbdf2cd8e3e846946d70bf0e
Kaustav Chanda <kstvkmrchanda2@gmail.com>
2025-09-24 22:18:15 +0530

    create ecs service

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -22,3 +22,24 @@ variable "db_username" {
 variable "db_password" {
   description = "Password for the Terraform database"
 }
+
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django"
+}
